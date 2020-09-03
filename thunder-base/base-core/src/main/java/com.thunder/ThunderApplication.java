8ee2f8c5774c7300f7461a5c1797bfcfdff51c48
liumiao <liumiao@noahgroup.com>
2020-09-03 17:42:25 +0800

    thunder-base初始化

---
+++ thunder-base/base-core/src/main/java/com.thunder/ThunderApplication.java
@@ -0,0 +1,16 @@
+package com.thunder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: llm
+ * @create: 2020/9/3
+ **/
+@SpringBootApplication
+public class ThunderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ThunderApplication.class, args);
+    }
+}
---
+++ thunder-dependencies/README.md
@@ -0,0 +1,1 @@
+thunder-dependencies 管理项目依赖版本
