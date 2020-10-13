7432b56af5b6e820beff688189de74686a90d0f7
liumiao <liumiao@noahgroup.com>
2020-10-13 18:04:40 +0800

    eureka add

---
+++ thunder-gateway/src/main/java/com/thunder/gateway/core/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.thunder.gateway.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: lim14905
+ * @create: 2020/10/13
+ **/
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
